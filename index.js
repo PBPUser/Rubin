6e1aa23fad1ef4518c2d04c2dca79b3e404fa767
PBPUser <43165341+PBPUser@users.noreply.github.com>
2018-09-12 16:08:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ robot.on("guildMemberAdd",function(member) {
     else
 
        if(servId == "340493689325486080"){ //DM
-        member.kick('Optional reason that will display in the audit logs').then(() => {
-          message.reply(`Вы успешно кикнули ${user.tag}`);
+        //member.kick('Optional reason that will display in the audit logs').then(() => {
+          //message.reply(`Вы успешно кикнули ${user.tag}`);
         })
         
             console.log("Joined to DM!");
